17882933f82cc1c8587a636300e3814bc766e8f2
Brooke Love <savienloveb@gmail.com>
2022-09-11 10:10:08 -0400

    creating build 01

--- build01.py
+++ build01.py
@@ -7,3 +7,6 @@ The following is on the console (i.e., preceded by a blank line):
 Greetings from a beginning Python programmer.
 Do you want to be addressed as ... 
 '''
+my_str = "\nGreeting from a beginning Python programmer. \nDo you want to be addressed as ...\n"
+
+print(my_str)
