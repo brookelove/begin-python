959622e565645e0e8d51cb6a3f370177585987bc
Brooke Love <savienloveb@gmail.com>
2022-09-11 10:22:44 -0400

    adding in build 04

--- build04.py
+++ build04.py
@@ -18,3 +18,44 @@ After a blank line, the same output, but applied to Archibald Montague Abercromb
 After a blank line, the same output, but applied to Cleopatra Anastasia Montgomery
 this will be using the format function
 '''
+# Postcondition 01
+my_str = "\nGreeting from a beginning Python programmer. \nDo you want to be addressed as ...\n"
+
+print(my_str)
+# Postcondition 02
+tabIn = ("....................======>")
+
+# put in the tabIn to ge the arrows then concat the beginning then use format function for name and concat a question mark 
+
+# Postcondition 03
+# Jane Margaret Doe
+first="Jane"
+middle = " Margaret"
+last = " Doe"
+
+# Postcondition 04
+print(tabIn + "{0}{1}{2} ?".format(first, middle, last))
+print(tabIn + "{0}{2}?".format(first, middle, last))
+print(tabIn + "Mr./Ms. {0}{1}{2}?".format(first, middle, last))
+print(tabIn + "Dear {0}".format(first, middle, last) + "? \n or")
+print(tabIn + "{2},{0}{1} ?\n".format(first, middle, last))
+
+# Archibald Montague Abercrombie
+first = "Archibald"
+middle = " Montague"
+last = " Abercrombie"
+print(tabIn + "{0}{1}{2} ?".format(first, middle, last))
+print(tabIn + "{0}{2}?".format(first, middle, last))
+print(tabIn + "Mr./Ms.{0}{1}{2}?".format(first, middle, last))
+print(tabIn + "Dear {0}".format(first, middle, last) + "? \n or")
+print(tabIn + "{2},{0}{1} ?\n".format(first, middle, last))
+
+# Cleopatra Anastasia Montgomery 
+first = "Cleopatra"
+middle = " Anastasia"
+last = " Montgomery"
+print(tabIn + "{0}{1}{2} ?".format(first, middle, last))
+print(tabIn + "{0}{2}?".format(first, middle, last))
+print(tabIn + "Mr./Ms. {0}{1}{2}?".format(first, middle, last))
+print(tabIn + "Dear {0}".format(first, middle, last) + "? \n or")
+print(tabIn + "{2},{0}{1} ?\n".format(first, middle, last))
