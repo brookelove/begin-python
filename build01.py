449807fead52d15f7df0eff4968964bf6d7b7b40
Brooke Love <savienloveb@gmail.com>
2022-09-11 10:12:26 -0400

    restructure build 01

--- build01.py
+++ build01.py
@@ -7,6 +7,6 @@ The following is on the console (i.e., preceded by a blank line):
 Greetings from a beginning Python programmer.
 Do you want to be addressed as ... 
 '''
-my_str = "\nGreeting from a beginning Python programmer. \nDo you want to be addressed as ...\n"
+my_str = "\nGreeting from a beginning Python programmer. \nDo you want to be addressed as ..."
 
 print(my_str)
